eacdd5b0ad58a43fdcc79bf56bcbcfc9d24f924f
Trent Nelson <trent@trent.me>
2025-11-06 20:09:30 -0800

    Update run-server.sh.

--- run-server.sh
+++ run-server.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-# Get the full path of this file.
-ROOT=$(cd $(dirname $0); pwd)
-# Add $ROOT/src/parallelopedia to PYTHONPATH.
-PYTHONPATH=$ROOT/src:$PYTHONPATH python -Xgil=0 -m parallelopedia.server --ip 0.0.0.0 --port 4444 --threads 40 --log-level DEBUG --debug
+python -Xgil=0 -m parallelopedia.http.server                                  \
+    --ip 0.0.0.0                                                              \
+    --port 4444                                                               \
+    --threads $(nproc)                                                        \
+    --log-level INFO                                                          \
+    --app-classes parallelopedia.http.server.PlaintextApp                     \
+                  parallelopedia.gpt2.Gpt2App                                 \
+                  parallelopedia.wiki.WikiApp                                 \
+                  parallelopedia.llm.CausalModelApp
+
